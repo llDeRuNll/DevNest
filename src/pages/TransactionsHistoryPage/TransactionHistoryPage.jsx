ad4c25ea50a721175bd489a50352d1dd8eed4168
sh1xzyy <alekslungol@gmail.com>
2025-05-07 17:34:52 +0300

    .

--- src/pages/TransactionsHistoryPage/TransactionHistoryPage.jsx
+++ src/pages/TransactionsHistoryPage/TransactionHistoryPage.jsx
@@ -1,5 +1,18 @@
-const TransactionHistoryPage = () => {
-  return <div>TransactionHistoryPage</div>;
-};
+import TransactionsList from '../../components/TransactionsList/TransactionsList '
+import TransactionsSearchTools from '../../components/TransactionsSearchTools/TransactionsSearchTools'
+import s from './TransactionsHistoryPage.module.css'
 
-export default TransactionHistoryPage;
+const TransactionHistoryPage = () => {
+	return (
+		<section className={s.section}>
+			<div className='container'>
+				<div className={s.transactionWrapper}>
+					<TransactionsSearchTools />
+					<TransactionsList />
+				</div>
+			</div>
+		</section>
+	)
+}
+
+export default TransactionHistoryPage
