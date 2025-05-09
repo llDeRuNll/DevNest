bf86fa052fb27878172106bed0968ff2b6393fa8
Pereverza Tetiana <tanyusha.vzletka82@gmail.com>
2025-05-10 00:06:22 +0300

    commit

--- src/components/BgImageWrapper/BgImageWrapper.jsx
+++ src/components/BgImageWrapper/BgImageWrapper.jsx
@@ -1,5 +1,11 @@
+import DecorationTab from "./DecorationTab";
+import s from "../BgImageWrapper.module.css";
 const BgImageWrapper = () => {
-  return <div>BgImageWrapper</div>;
+  return (
+    <div className={s.wrapper}>
+      <div className={s.backgroundImage}></div>
+      <DecorationTab />
+    </div>
+  );
 };
-
 export default BgImageWrapper;
