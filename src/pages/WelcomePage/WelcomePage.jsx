6cee01eb56a2402fb10bf4f2c57b0d6477c84f8e
Pereverza Tetiana <tanyusha.vzletka82@gmail.com>
2025-05-09 17:28:24 +0300

    finish marking

--- src/pages/WelcomePage/WelcomePage.jsx
+++ src/pages/WelcomePage/WelcomePage.jsx
@@ -3,31 +3,42 @@ import s from "./WelcomePage.module.css";
 import AuthNav from "../../components/AuthNav/AuthNav";
 import AllUsersTab from "../../components/AllUsersTab/AllUsersTab";
 import { HiMiniArrowUpRight } from "react-icons/hi2";
+import useIsDesktop from "../../hooks/useIsDesktop";
 
 const WelcomePage = () => {
+  const isDesktop = useIsDesktop();
+
   return (
-    <div className={s.container}>
+    <div className="container">
       <main className={s.main}>
         <div className={s.textSection}>
           <div className={s.listInfo}>
             <p className={s.subheading}>EXPENSE LOG</p>
+
             <h2 className={s.heading}>
-              Manage {""}
-              <span className={s.noBreak}>
-                Your <span className={s.spanHeading}>Finances</span>
+              Manage Your{" "}
+              <span className={s.breakMobile}>
+                <span className={s.spanHeading}>Finances</span>
+                <br />
+                Masterfully!
               </span>
-              <br />
-              Masterfully!
+              <span className={s.breakTabletDesktop}>
+                <br />
+                <span className={s.spanHeading}>Finances</span> Masterfully!
+              </span>
             </h2>
+
             <p className={s.description}>
               ExpenseTracker effortlessly empowers you to take control of your
               finances! With intuitive features, it simplifies the process of
               tracking and managing expenses, allowing for a stress-free mastery
               over your financial world.
             </p>
+
             <AuthNav />
           </div>
-          <AllUsersTab />
+
+          {isDesktop && <AllUsersTab />}
         </div>
 
         <div className={s.imageSection}>
