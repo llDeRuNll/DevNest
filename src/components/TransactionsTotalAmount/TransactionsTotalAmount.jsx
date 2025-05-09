a5d8c216ba474ab257e5c0ff252c3b47e6aa114a
Halyna Moldovianu <Beorgeos@gmail.com>
2025-05-09 19:18:38 +0200

    Toaster added

--- src/components/TransactionsTotalAmount/TransactionsTotalAmount.jsx
+++ src/components/TransactionsTotalAmount/TransactionsTotalAmount.jsx
@@ -2,7 +2,6 @@ import s from "./TransactionsTotalAmount.module.css";
 import TotalAmountElement from "../TotalAmountElement/TotalAmountElement";
 import { useSelector } from "react-redux";
 import { selectTransactionsTotal } from "../../redux/transactions/selectors";
-import ToasterSuccess from "../ToasterSuccess/ToasterSuccess";
 
 const TransactionsTotalAmount = () => {
   const { incomes, expenses } = useSelector(selectTransactionsTotal);
@@ -11,9 +10,6 @@ const TransactionsTotalAmount = () => {
 
   return (
     <div className={s.mainWrapper}>
-      <button onClick={ToasterSuccess} type="button">
-        Click
-      </button>
       <TotalAmountElement
         currency={currency}
         amount={incomes}
