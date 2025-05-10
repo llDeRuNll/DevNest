8ce340eb389cf1bb56548072bfbb4907773dcf7b
Nikita Naskalnyi <niknnm06@gmail.com>
2025-05-10 22:20:20 +0200

    add adaptive design, change logic in AuthForm

--- src/components/AuthForm/AuthForm.jsx
+++ src/components/AuthForm/AuthForm.jsx
@@ -1,6 +1,63 @@
 import React from "react";
-import { Formik, Form, Field, ErrorMessage } from "formik";
+import { Formik, Form, useField } from "formik";
+import { Link } from "react-router-dom";
 import s from "./AuthForm.module.css";
+
+import {
+  AiOutlineCheckCircle,
+  AiOutlineCloseCircle,
+  AiOutlineEye,
+  AiOutlineEyeInvisible,
+} from "react-icons/ai";
+
+const CustomInput = ({ type = "text", ...props }) => {
+  const [field, meta] = useField(props);
+  const [showPassword, setShowPassword] = React.useState(false);
+
+  const isError = meta.touched && meta.error;
+  const isSuccess = meta.touched && !meta.error;
+  const isPassword = type === "password";
+
+  const inputClass = `${s.authInput} ${
+    isError ? s.inputError : isSuccess ? s.inputSuccess : ""
+  }`;
+
+  return (
+    <div className={s.inputWrapper}>
+      <input
+        className={inputClass}
+        type={isPassword && showPassword ? "text" : type}
+        {...field}
+        {...props}
+      />
+
+      {isPassword && (
+        <span
+          className={s.iconRight}
+          onClick={() => setShowPassword(!showPassword)}
+        >
+          {showPassword ? <AiOutlineEye /> : <AiOutlineEyeInvisible />}
+        </span>
+      )}
+
+      {!isPassword && isError && (
+        <span className={s.iconRightError}>
+          <AiOutlineCloseCircle />
+        </span>
+      )}
+      {!isPassword && isSuccess && (
+        <span className={s.iconRightSuccess}>
+          <AiOutlineCheckCircle />
+        </span>
+      )}
+      {isPassword && isSuccess && (
+        <div className={s.successMessage}>Password is secure</div>
+      )}
+
+      {isError && <div className={s.authError}>{meta.error}</div>}
+    </div>
+  );
+};
 
 const AuthForm = ({
   fields = [],
@@ -13,86 +70,70 @@ const AuthForm = ({
   navigationText,
   navigationLinkText,
   navigationLinkHref,
-  error,
   isLoading,
+  variant = "login",
 }) => {
+  const buttonClass =
+    variant === "register"
+      ? `${s.authBtn} ${s.btnRegister}`
+      : `${s.authBtn} ${s.btnLogin}`;
   return (
     <div className={s.authContainer}>
-      <h2 className={s.authTitle}>{title}</h2>
-      <p className={s.authDescription}>{description}</p>
+      <div className={s.authBgWrapper}>
+        <img
+          src="../../../imgWelcomePage/Rec2x.png"
+          alt="background image"
+          className={s.authBg}
+        />
+      </div>
+      <div className={s.authFormContainer}>
+        <h2 className={s.authTitle}>{title}</h2>
+        <p className={s.authDescription}>{description}</p>
 
-      <Formik
-        initialValues={initialValues}
-        validationSchema={validationSchema}
-        onSubmit={(values, actions) => {
-          onSubmit(values);
-          actions.setSubmitting(false);
-        }}
-      >
-        {({ isSubmitting }) => (
-          <Form className={s.authForm}>
-            {fields.includes("name") && (
-              <>
-                <Field name="name" placeholder="Name" className={s.authInput} />
-                <ErrorMessage
-                  name="name"
-                  component="div"
-                  className={s.authError}
-                />
-              </>
-            )}
+        <Formik
+          initialValues={initialValues}
+          validationSchema={validationSchema}
+          onSubmit={(values, actions) => {
+            onSubmit(values);
+            actions.setSubmitting(false);
+          }}
+        >
+          {({ isSubmitting }) => (
+            <Form className={s.authForm}>
+              {fields.includes("name") && (
+                <CustomInput name="name" placeholder="Name" />
+              )}
 
-            {fields.includes("email") && (
-              <>
-                <Field
-                  name="email"
-                  type="email"
-                  placeholder="Email"
-                  className={s.authInput}
-                />
-                <ErrorMessage
-                  name="email"
-                  component="div"
-                  className={s.authError}
-                />
-              </>
-            )}
+              {fields.includes("email") && (
+                <CustomInput name="email" type="email" placeholder="Email" />
+              )}
 
-            {fields.includes("password") && (
-              <>
-                <Field
+              {fields.includes("password") && (
+                <CustomInput
                   name="password"
                   type="password"
                   placeholder="Password"
-                  className={s.authInput}
                 />
-                <ErrorMessage
-                  name="password"
-                  component="div"
-                  className={s.authError}
-                />
-              </>
-            )}
+              )}
 
-            {error && <p className={s.authError}>{error}</p>}
+              <button
+                type="submit"
+                disabled={isSubmitting || isLoading}
+                className={buttonClass}
+              >
+                {isLoading ? "Loading..." : buttonText}
+              </button>
+            </Form>
+          )}
+        </Formik>
 
-            <button
-              type="submit"
-              disabled={isSubmitting || isLoading}
-              className={s.authBtn}
-            >
-              {isLoading ? "Loading..." : buttonText}
-            </button>
-          </Form>
-        )}
-      </Formik>
-
-      <p className={s.authText}>
-        {navigationText}{" "}
-        <a href={navigationLinkHref} className={s.authLink}>
-          {navigationLinkText}
-        </a>
-      </p>
+        <p className={s.authText}>
+          {navigationText}{" "}
+          <Link to={navigationLinkHref} className={s.authLink}>
+            {navigationLinkText}
+          </Link>
+        </p>
+      </div>
     </div>
   );
 };
