388edd9f1f4cbddeacdb6a9c5bd1c5002375051e
FounDy <vkadgcundgd@gmail.com>
2025-05-11 19:28:10 +0200

    Сохраняю изменения перед мержем

--- src/components/UserSetsModal/UserSetsModal.jsx
+++ src/components/UserSetsModal/UserSetsModal.jsx
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import s from "./UserSetsModal.module.css";
 import { LuUser } from "react-icons/lu";
+
 function UserSetsModal({ onClose }) {
+  const fileInputRef = useRef(null);
+  const [avatar, setAvatar] = useState(null);
+
+  const handleUploadClick = () => {
+    fileInputRef.current.click();
+  };
+
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      const imageURL = URL.createObjectURL(file);
+      setAvatar(imageURL);
+    }
+  };
+
+  const handleRemove = () => {
+    setAvatar(null);
+    fileInputRef.current.value = null;
+  };
+
   return (
     <div className={s.overlay} onClick={onClose}>
       <div className={s.modal} onClick={(e) => e.stopPropagation()}>
@@ -12,13 +33,28 @@ function UserSetsModal({ onClose }) {
 
         <div className={s.avatarSection}>
           <div className={s.avatarCircle}>
-            <span className={s.avatarIcon}>
-              <LuUser />
-            </span>
+            {avatar ? (
+              <img src={avatar} alt="Avatar" className={s.avatarImg} />
+            ) : (
+              <span className={s.avatarIcon}>
+                <LuUser />
+              </span>
+            )}
           </div>
           <div className={s.avatarBtns}>
-            <button className={s.grayBtn}>Upload new photo</button>
-            <button className={s.grayBtn}>Remove</button>
+            <button className={s.grayBtn} onClick={handleUploadClick}>
+              Upload new photo
+            </button>
+            <button className={s.grayBtn} onClick={handleRemove}>
+              Remove
+            </button>
+            <input
+              type="file"
+              accept="image/*"
+              ref={fileInputRef}
+              style={{ display: "none" }}
+              onChange={handleFileChange}
+            />
           </div>
         </div>
 
@@ -28,7 +64,6 @@ function UserSetsModal({ onClose }) {
             <option value="usd">$ USD</option>
             <option value="eur">€ EUR</option>
           </select>
-
           <input type="text" className={s.input} placeholder="Alex Rybachok" />
         </div>
 
