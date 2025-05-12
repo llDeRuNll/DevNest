a08270a0bbd7542fcfbf2763923c910e918da0d8
FounDy <vkadgcundgd@gmail.com>
2025-05-12 03:44:31 +0200

    problem_with_redux

--- src/components/UserPanel/UserPanel.jsx
+++ src/components/UserPanel/UserPanel.jsx
@@ -5,24 +5,40 @@ import s from "./UserPanel.module.css";
 import { userLogout } from "../../redux/auth/operations";
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
 
 function UserPanel({ onProfileClick }) {
   const dispatch = useDispatch();
   const navigate = useNavigate();
+
   const handleLogout = async () => {
-    await dispatch(userLogout()).unwrap();
-    navigate("/");
+    try {
+      await dispatch(userLogout()).unwrap();
+      navigate("/");
+      toast.success("You have successfully logged out!");
+    } catch (error) {
+      toast.error("Error logging out.");
+    }
   };
+
   return (
     <div className={s.dropdownMenu}>
       <div>
-        <button className={s.bthnIcon} onClick={onProfileClick}>
+        <button
+          className={s.bthnIcon}
+          onClick={onProfileClick}
+          aria-label="Open profile settings"
+        >
           <LuUser className={s.icon} />
           Profile settings
         </button>
       </div>
       <div>
-        <button className={s.bthnIcon} onClick={handleLogout}>
+        <button
+          className={s.bthnIcon}
+          onClick={handleLogout}
+          aria-label="Log out"
+        >
           <CiLogout className={s.icon} />
           Log out
         </button>
