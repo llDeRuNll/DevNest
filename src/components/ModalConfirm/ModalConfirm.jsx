1f1ab4cf914245c5c833dd4b62c482477d119381
sh1xzyy <alekslungol@gmail.com>
2025-05-12 13:50:59 +0300

    context for modal and transaction user experience

---
+++ src/components/ModalConfirm/ModalConfirm.jsx
@@ -0,0 +1,44 @@
+import { useModal } from '../../utils/Modal/useModal'
+import s from './ModalConfirm.module.css'
+import { IoMdClose } from 'react-icons/io'
+
+const ModalConfirm = ({ title, confirmButton, confirmFc }) => {
+	const { closeModal } = useModal()
+
+	const handleOverlayClick = event => {
+		if (event.target === event.currentTarget) {
+			closeModal()
+		}
+	}
+
+	return (
+		<div className={s.modalOverlay} onClick={handleOverlayClick}>
+			<div className='container'>
+				<div className={s.modal}>
+					<button className={s.closeButton} onClick={() => closeModal()}>
+						<IoMdClose className={s.closeIcon} />
+					</button>
+					<p className={s.modalTitle}>{title}</p>
+					<div className={s.buttonsWrapper}>
+						<button
+							className={s.logoutButton}
+							type='button'
+							onClick={confirmFc}
+						>
+							{confirmButton}
+						</button>
+						<button
+							className={s.cancelButton}
+							type='button'
+							onClick={() => closeModal()}
+						>
+							Cancel
+						</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default ModalConfirm
