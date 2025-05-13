e10113e426d9390089dc96866d173000bf3a0b38
llDeRuNll <eduardprudiys@gmail.com>
2025-05-13 04:19:01 -0400

    fix loader and modal

--- src/components/UserPanel/UserPanel.jsx
+++ src/components/UserPanel/UserPanel.jsx
@@ -1,57 +1,51 @@
-import React, { useState } from "react";
 import { LuUser } from "react-icons/lu";
 import { CiLogout } from "react-icons/ci";
-import s from "./UserPanel.module.css";
-import { userLogout } from "../../redux/auth/operations";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
+import { useModal } from "../../utils/Modal/useModal";
+import ModalConfirm from "../ModalConfirm/ModalConfirm";
 import Loader from "../Loader/Loader";
+import { userLogout } from "../../redux/auth/operations";
+
+import s from "./UserPanel.module.css";
+import { selectIsLoading } from "../../redux/transactions/selectors";
 
 function UserPanel({ onProfileClick }) {
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const [isLoggingOut, setIsLoggingOut] = useState(false);
+  const { isModalOpen, openModal, closeModal } = useModal();
 
-  const handleLogout = async () => {
+  const isLoggingOut = useSelector(selectIsLoading);
+
+  const handleConfirmLogout = async () => {
     try {
-      setIsLoggingOut(true);
       await dispatch(userLogout()).unwrap();
+      closeModal();
       navigate("/");
       toast.success("You have successfully logged out!");
-    } catch (error) {
-      toast.error(error, "Error logging out.");
-    } finally {
-      setIsLoggingOut(false);
+    } catch {
+      toast.error("Error logging out.");
     }
   };
 
   return (
-    <div className={s.panelContainer}>
+    <>
       {isLoggingOut && (
         <div className={s.loaderOverlay}>
           <Loader />
         </div>
       )}
-      <div className={s.dropdownMenu} aria-hidden={isLoggingOut}>
+
+      <div className={s.dropdownMenu}>
         <div>
-          <button
-            className={s.btnIcon}
-            onClick={onProfileClick}
-            aria-label="Open profile settings"
-            disabled={isLoggingOut}
-          >
+          <button className={s.bthnIcon} onClick={onProfileClick}>
             <LuUser className={s.icon} />
             Profile settings
           </button>
         </div>
         <div>
-          <button
-            className={s.btnIcon}
-            onClick={handleLogout}
-            aria-label="Log out"
-            disabled={isLoggingOut}
-          >
+          <button className={s.bthnIcon} onClick={openModal}>
             <CiLogout className={s.icon} />
             Log out
           </button>
@@ -66,7 +60,6 @@ function UserPanel({ onProfileClick }) {
         />
       )}
     </>
-
   );
 }
 
