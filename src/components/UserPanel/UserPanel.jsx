516d76fa9df3cecd2f8b006bb605b8d76f98c5f4
FounDy <vkadgcundgd@gmail.com>
2025-05-13 10:08:07 +0200

    dsaasd

--- src/components/UserPanel/UserPanel.jsx
+++ src/components/UserPanel/UserPanel.jsx
@@ -41,6 +41,13 @@ function UserPanel({ onProfileClick }) {
           </button>
         </div>
       </div>
+      {isModalOpen && (
+        <ModalConfirm
+          title="Are you sure you want to log out?"
+          confirmButton="Yes, log out"
+          confirmFc={handleConfirmLogout}
+        />
+      )}
     </>
   );
 }
